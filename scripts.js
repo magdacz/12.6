651379e87d7fb78e1a26b02e49f26edd8022c7c9
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-11-17 22:14:45 +0100

    Change style.css, change js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,30 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
 
-	$('#search').click(searchCountries);
 
-	function searchCountries() {
-	 	var countryName = $('#country-name').val();
-	if(!countryName.length) countryName = 'Poland';
-	$.ajax({
-	  		url: url + countryName,
-	  		method: 'GET',
-	  		success: showCountriesList
-	  	});
-	}
+function showCountriesList(resp) {
+    countriesList.empty(); //wyczyszczenie listy krajów po poprzednim zapytaniu
+    resp.forEach(function(item) {
+        $('<li>').text(item.name).appendTo(countriesList);
+        $('<h3>').text('Background information').appendTo(countriesList);
+        $('<p>').text(`Capital: ${item.capital}`).appendTo(countriesList);
+        $('<p>').text(`Land area: ${item.area}`).appendTo(countriesList);
+        $('<p>').text(`Population: ${item.population}`).appendTo(countriesList);
+        $('<p>').text(`Language(s): ${item.languages}`).appendTo(countriesList);
+        $('<p>').text(`Currency: ${item.currencies}`).appendTo(countriesList);
+    })
+}
 
-	function showCountriesList(resp) {
-	    countriesList.empty();
-	    resp.forEach(function(item) {
-	        $('<li>').text(item.name).appendTo(countriesList);
-	    });
-	}
+
+function searchCountries() {
+    var countryName = $('#country-name').val(); //val() zwraca lub ustawia wartość właściwości wybranego elementu.
+    if(!countryName.length) countryName = 'Poland';
+    $.ajax({
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
+    });
+}
+
+
+$('#search').click(searchCountries);
